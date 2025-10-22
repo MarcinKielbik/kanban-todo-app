1622ab890740fd45b536238f7bb831a92e126ede
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-10-22 21:19:19 +0200

    feat: implement todo page

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoPage: React.FC = () => {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>To-Do Lista</h2>
+      <p>Tutaj będzie Twoja lista zadań ✅</p>
+    </div>
+  );
+};
+
+export default TodoPage;
